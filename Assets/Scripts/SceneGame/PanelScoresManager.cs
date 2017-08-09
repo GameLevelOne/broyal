1f6dd42bf8ac02130c06965fab677c3bfc591697
Helga <helga@gamelevelone.com>
2017-08-09 18:20:39 +0700

    fix result text

--- Assets/Scripts/SceneGame/PanelScoresManager.cs
+++ Assets/Scripts/SceneGame/PanelScoresManager.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PanelScoresManager : MonoBehaviour {
 	public GameObject panelGameReady;
 	public GameObject panelScore;
 	public GameObject panelScoreboard;
 	public GameObject panelLoadingBar;
+	public Text textResult;
 
-	public void OnClickNext (){
+	bool rightAnswer = false;
+
+	public bool RightAnswer{ set { rightAnswer=value;}}
+
+	public void OnClickNext ()
+	{
+		if (rightAnswer) {
+			textResult.text = "YOU PASSED!";
+		} else {
+			textResult.text = "YOU LOSE!";
+		}
+
 		panelScoreboard.SetActive(true);
 		panelScore.SetActive(false);
 	}
